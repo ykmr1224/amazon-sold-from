08b89f99cb8d854534c007f42cabbd9d3a2eaef7
ykmr1224 <ykmr1224@gmail.com>
2020-01-02 15:39:17 +0900

    Rename constants in logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,5 @@
-const noop = () => {};
-const logLevels = {
+const NOOP = () => {};
+const LOG_LEVELS = {
   debug: {level: 0, method: console.debug},
   info: {level: 1, method: console.info},
   warn:{level: 2, method: console.warn},
@@ -8,11 +8,11 @@ const logLevels = {
 
 let getLogger = (threshold) => {
   let result = {};
-  for (let name in logLevels) {
-    let level = logLevels[name];
-    result[name] = (level.level >= threshold.level) ? level.method : noop;
+  for (let name in LOG_LEVELS) {
+    let level = LOG_LEVELS[name];
+    result[name] = (level.level >= threshold.level) ? level.method : NOOP;
   }
   return result;
 };
 
-let log = getLogger(document.cookie.includes('debug=1') ? logLevels.debug : logLevels.info);
+let log = getLogger(document.cookie.includes('debug=1') ? LOG_LEVELS.debug : LOG_LEVELS.info);
